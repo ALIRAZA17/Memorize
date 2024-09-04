ae75f955e917bb8083d33d2f166f68fb8f4ce5b7
ALIRAZA17 <ialidar2001@gmail.com>
2024-09-04 16:54:54 +0500

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Muhammad Ali Raza Dar on 04/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
