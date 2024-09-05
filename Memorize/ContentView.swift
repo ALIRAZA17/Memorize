f7ce3bf5697af977d992ce6bc8373b96cc4922ad
ALIRAZA17 <ialidar2001@gmail.com>
2024-09-05 15:48:57 +0500

    isFaceUp ToggelAbles

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -22,23 +22,22 @@ struct ContentView: View {
 }
 
 struct CardView: View {
-    var isFaceUp: Bool = false
+    @State var isFaceUp = false
     var body: some View {
-        if isFaceUp {
-            ZStack{
-                RoundedRectangle(cornerRadius: 12)
+        let base = RoundedRectangle(cornerRadius: 12)
+        ZStack{
+            if isFaceUp{
+                base
             }
-        }
-        else
-        {
-            ZStack{
-                RoundedRectangle(cornerRadius: 12).foregroundColor(.white)
-                RoundedRectangle(cornerRadius: 12)
-                    .strokeBorder(lineWidth: 2)
+            else{
+                base.foregroundColor(.white)
+                base.strokeBorder(lineWidth: 2)
                 Text("🤣")
             }
         }
-        
+        .onTapGesture {
+            isFaceUp.toggle()
+        }
     }
 }
 #Preview {
